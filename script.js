86f43f51c1d096b2160f4a4746754d69ac363e87
Vadym Zelenko <115219323+01cheese@users.noreply.github.com>
2024-09-28 14:35:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,8 @@ function loadMarkdown(filename) {
     fetch(filename)
         .then(response => response.text())
         .then(text => {
-            const markdownContent = marked(text); // Конвертируем Markdown в HTML с помощью marked.js
+            const markdownContent = marked.parse(text);  // Используем marked.parse()
             document.getElementById('markdown-content').innerHTML = markdownContent;
         })
         .catch(error => console.error('Ошибка загрузки файла:', error));
 }
-
-// Загрузка первой части книги при загрузке страницы
-window.onload = function() {
-    loadMarkdown('PART_1.md');
-};
